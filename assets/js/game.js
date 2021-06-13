5fda1cd8eb1a84e3248fd74decfeec840ce75fa1
Lashaun <clarklas831@gmail.com>
2021-06-13 10:20:41 -0500

    move onto next robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,6 +51,7 @@ var fight = function(enemyName) {
         }}}
 
       for (var i = 0; i < enemyNames.length; i++) {
-        debugger;
-        fight(enemyNames[i])
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
       }
